1ecad69fb7233b55bdc984616d455665eae219e7
Valsinev <streamline_@abv.bg>
2024-12-03 23:00:59 +0200

    added configuration options rounding mode and decimal scale

--- src/main/java/org/example/configuration/IConfiguration.java
+++ src/main/java/org/example/configuration/IConfiguration.java
@@ -1,6 +1,7 @@
 package org.example.configuration;
 
 import java.awt.*;
+import java.math.RoundingMode;
 import java.util.Optional;
 
 public interface IConfiguration {
@@ -13,6 +14,10 @@ public interface IConfiguration {
     long getDailyMoneyWithHotel();
 
     int getNumberOfDaysInOneOrder();
+
+    int getScale();
+
+    RoundingMode getRoundingMode();
 
     Optional<String> orderImgResourcePath();
 
